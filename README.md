4481c35aa2ff6cda7316cb6ad8811e89a4ed58e6
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2022-10-08 18:01:14 +0100

    Implemented keyboard functionality

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wordle Clone v1.0
+
+## Project specifications
+
+This is a clone of the popular Web-based game called Wordle. It will be written in pure HTML, CSS and JavaScript, and will have much of the same functionality. This project was started on _Thursday, 6th October 2022_ and was authored by _Eti-ini Bernard Effiong-Robert._
+
+---
+
+## Project Stack
+
+1. HTML
+2. CSS
+3. JavaScript
+4. Git/GitHub, for version control and cloud repository
+
--- wordle.js
+++ wordle.js
@@ -2,11 +2,23 @@ let possible_words = data;
 let word_index = Math.floor(Math.random() * (possible_words.length + 1));
 let entire_word = possible_words[word_index];
 let word = [];
-
+let userHasGuessedCorrectly = false;
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 for (let letter of entire_word) {
     word.push(letter);
 }
 
+window.addEventListener("keydown", event => {
+    if (event.key == "Enter") {
+        submitGuess();
+    }
+    if (event.key == "Backspace") {
+        deleteLetter();
+    }
+    if (LETTERS.includes(event.key.toUpperCase())) {
+        writeLetter(event.key.toUpperCase());
+    }
+})
 console.log(word);
 
 let currentGuess = [];
@@ -14,9 +26,9 @@ let currentRow = 1;
 
 function writeLetter(letter) {
     /* Each letter key in the HTML page passes itself as a value to this function */
-    if (currentRow < 7) {
+    if (currentRow < 7 && userHasGuessedCorrectly == false) {
         // If there are still blank guesses remaining
-        if (currentGuess.length != 5) {
+        if (currentGuess.length < 5) {
             // If user has not guessed up to five letters
 
             currentGuess.push(letter);
@@ -39,6 +51,11 @@ function deleteLetter() {
 }
 function submitGuess() {
     if (currentGuess.length == 5) {
+        if (currentGuess == word) {
+            userHasGuessedCorrectly = true;
+            endGame();
+            return;
+        }
         // Checks if the user has guessed a full word
         for (let i=1; i < 6; i++) {
             // Iterates over all the squares in a guess row
@@ -57,7 +74,11 @@ function submitGuess() {
         currentRow++;   // Begins guessing the next row
         currentGuess = [];
         if (currentRow == 7) {
-            showWord();
+            if (userHasGuessedCorrectly == false) {
+                showWord();
+            } else {
+                endGame();
+            }
         }
     }
 }
@@ -70,4 +91,8 @@ function showWord() {
     message.style.animationName = "slide-down";
     setInterval(() => {message.style.animationName = "slide-up";}, 1500);
     setInterval(() => {message.style.display = "none";}, 1900);
+}
+
+function endGame() {
+    console.log("You got it right!");
 }
